736b96bd360fc03d35461f1fc727efeb7f41fc19
AmitV <88198146+Digitup1@users.noreply.github.com>
2026-03-27 15:50:13 +0530

    Initialize main.jsx with React application setup

---
+++ main.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import App from './overview_screen_v_5_intelligence_led.jsx'
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+)
