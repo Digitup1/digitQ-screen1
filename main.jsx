0bd95b36f4a02e2bfad5d02c2d72ddaf731bda71
AmitV <88198146+Digitup1@users.noreply.github.com>
2026-03-27 16:01:43 +0530

    Update main.jsx

--- main.jsx
+++ main.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './Overview_screen_v_5_intelligence_led.jsx'
+import App from './overview_screen_v_5_intelligence_led.jsx'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
